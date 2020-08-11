3ded47ffd8423dcbb4ed41e3662009f5ffcf343b
afiatamanna06 <afiatamanna06@gmail.com>
2020-08-11 20:10:41 +0600

    Header.js added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import {
+    Header
+} from './components/Header';
 import './App.css';
 
 function App() {
     return ( <
-        div > Hello World <
+        div > < Header / > <
         /div>
     );
 }
