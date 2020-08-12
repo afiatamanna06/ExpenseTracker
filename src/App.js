475683e85176d132856ce07850a95ad6490122ee
afiatamanna06 <afiatamanna06@gmail.com>
2020-08-12 19:35:03 +0600

    Balance.js added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import {
     Header
 } from './components/Header';
+import { Balance } from './components/Balance';
 import './App.css';
 
 function App() {
     return ( <
-        div > < Header / > <
+        div > < Header / > < Balance / > <
         /div>
     );
 }
